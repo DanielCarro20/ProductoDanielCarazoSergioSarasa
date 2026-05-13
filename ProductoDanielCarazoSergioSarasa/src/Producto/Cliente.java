586a7419d252b451bb26b72423a6f2134334d974
molu27 <1DAM@A11-PC25>
2026-05-13 12:42:15 +0200

    alumno2

--- ProductoDanielCarazoSergioSarasa/src/Producto/Cliente.java
+++ ProductoDanielCarazoSergioSarasa/src/Producto/Cliente.java
@@ -1,5 +1,36 @@
 package Producto;
 
 public class Cliente {
+	protected int id;
+	protected String direccion;
+	
+	public Cliente(int id, String direccion) {
+		super();
+		this.id = id;
+		this.direccion = direccion;
+	}
+	public Cliente(String direccion) {
+		super();
+		this.direccion = direccion;
+	}
+	public Cliente() {
+		super();
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getDireccion() {
+		return direccion;
+	}
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+	@Override
+	public String toString() {
+		return "Cliente [id=" + id + ", direccion=" + direccion + "]";
+	}
 
 }
